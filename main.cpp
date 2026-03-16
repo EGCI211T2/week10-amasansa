ed2784f048c7b0070064179e48cec789f809e6d8
asnasama <asnabbb@gmail.com>
2026-03-16 08:39:09 +0000

    Updated function main.c

--- main.cpp
+++ main.cpp
@@ -12,12 +12,15 @@ using namespace std;
 
 int main(){
     struct Time t1,t2,t3;
-    cout<<"What time was it?";
+    // cout<<"What time was it?";
+    cout << "t1 ";
     getTime(t1);
-    cout<<"What time is it now?";
+    // cout<<"What time is it now?";
+    cout << "t2 ";
     getTime(t2);
     t3=subtract(t2,t1); //t3=t2-t1
-    cout<<"Time diff is ";
+    // cout<<"Time diff is ";
+    cout << "t3 ";
     display(t3);
 
     // float age;
