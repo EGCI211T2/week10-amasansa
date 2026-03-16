49695b7fbf02d7d225152f3303ecac3d5632a2f7
asnasama <asnabbb@gmail.com>
2026-03-16 08:27:01 +0000

    completed function Time.h

--- Time.h
+++ Time.h
@@ -2,9 +2,38 @@ struct Time{
 	int m,h,s;
 };
 
-
 //create 3 functions
 
 //1.  getTime function
 //2. subtract fuctions
 //3. display functions
+
+void getTime(Time &t)
+{
+	cin >>t.h >>t.m >>t.s;
+}
+
+Time subtract(Time t2,Time t1)
+{
+	int sec1=(t1.h*3600)+(t1.m*60)+t1.s;
+	int sec2=(t2.h*3600)+(t2.m*60)+t2.s;
+	int diff=sec2-sec1;
+
+	if(diff<0)
+	{
+		diff+=86400; //24hrs
+	}
+	Time t3;
+	t3.h=diff/3600;
+	diff%=3600;
+	t3.m=diff/60;
+	diff%=60;
+	t3.s=diff;
+
+	return t3;
+}
+
+void display(Time t3)
+{
+	cout <<setfill('0') <<setw(2) <<t3.h <<":" <<setfill('0') <<setw(2) <<t3.m <<":" <<setfill('0') <<setw(2) <<t3.s <<endl;
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 
 #include "Time.h"
+// void square(float&);
+// void square(float &x)
+// {
+//     x*=x;
+// }
 
 int main(){
     struct Time t1,t2,t3;
@@ -14,4 +19,21 @@ int main(){
     t3=subtract(t2,t1); //t3=t2-t1
     cout<<"Time diff is ";
     display(t3);
+
+    // float age;
+    // string colour;
+    // cout << "How old are you? and What your fav colour?" << endl;
+    // cin >> age >> colour;
+    // cout << "Your age is " << setw(10) << age << endl;
+    // cout <<"Your fav colour is " << setw(5) << colour << endl << endl;
+
+    // cout << "Your age is " << setfill(' ') << setw(10) << age << endl;
+    // cout <<"Your fav colour is " << setw(5) << colour << endl << endl;
+
+    // cout << "Your age is " << setw(10) << fixed << setprecision(2) << age << endl;
+    // cout <<"Your fav colour is " << setw(5) << colour << endl << endl;
+
+    // square(age);
+    // cout << "Your age is " << setw(10) << fixed << setprecision(2) << age << endl;
+    // cout <<"Your fav colour is " << setw(5) << colour << endl;
 }
